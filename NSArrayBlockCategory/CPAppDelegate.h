7aac826ed4b3b5dcfe589e07703088c1637d149a
Gabriele Petronella <gabriele.petronella@gmail.com>
2013-01-02 17:06:47 +0100

    Initial Commit

---
+++ NSArrayBlockCategory/CPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CPAppDelegate.h
+//  NSArrayBlockCategory
+//
+//  Created by Gabriele Petronella on 1/2/13.
+//  Copyright (c) 2013 CodePills. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NSArrayBlockCategoryTests/NSArrayBlockCategoryTests.h
@@ -0,0 +1,13 @@
+//
+//  NSArrayBlockCategoryTests.h
+//  NSArrayBlockCategoryTests
+//
+//  Created by Gabriele Petronella on 1/2/13.
+//  Copyright (c) 2013 CodePills. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface NSArrayBlockCategoryTests : SenTestCase
+
+@end
